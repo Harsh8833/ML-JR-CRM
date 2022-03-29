a7e9c2c8c719564aff0acd9e3bc39887fa5c6daf
Dewansh07 <dewansh.2021@vitbhopal.ac.in>
2022-03-29 12:48:44 +0530

    settings

--- crm/settings.py
+++ crm/settings.py
@@ -105,17 +105,23 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / 'static')
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+MEDIA_URL= "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
