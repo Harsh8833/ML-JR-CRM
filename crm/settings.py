1bedb8657328247259ad0cda17639560fa5479b6
Dewansh07 <dewansh.2021@vitbhopal.ac.in>
2022-03-29 22:54:59 +0530

    admin

--- crm/settings.py
+++ crm/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'newbook'
 ]
 
 MIDDLEWARE = [
@@ -116,12 +117,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'static')
-    ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 MEDIA_URL= "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
