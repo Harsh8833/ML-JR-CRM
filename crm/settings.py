cd40ed046fe05af68a14b20a4b9d290eb87e508d
Harsh Raj <harsh.raj8833@gmail.com>
2022-03-30 17:12:44 +0530

    existing booking fixed

--- crm/settings.py
+++ crm/settings.py
@@ -119,6 +119,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / 'static')
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL= "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
