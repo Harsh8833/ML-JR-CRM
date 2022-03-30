f4619c119673c5ab13a9f4df2b1d8d473cc7980a
Dewansh07 <dewansh.2021@vitbhopal.ac.in>
2022-03-30 19:40:29 +0530

    databse fix

--- crm/urls.py
+++ crm/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from booking import views
+from newbooking import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
