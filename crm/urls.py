5918c79e355ab633fb5faa48a633301a22294f68
Dewansh07 <dewansh.2021@vitbhopal.ac.in>
2022-03-30 14:30:01 +0530

    url fixed

--- crm/urls.py
+++ crm/urls.py
@@ -19,9 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from crm import views
+from booking import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('menu.urls'))
+    path('', include('menu.urls')),
+    path('newbooking/', views.newbooking ),
+    path('existingbooking/', views.existingbooking ),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
