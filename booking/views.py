d3d4bd86267f4b449d79a81e44ca28d7d4b9fb22
Harsh Raj <harsh.raj8833@gmail.com>
2022-03-30 23:30:29 +0530

    login required

--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
 def newbooking(request):
     return render(request, 'new_booking.html')
 
+
+@login_required
 def existingbooking(request):
     return render(request, 'existing_booking.html')
--- crm/urls.py
+++ crm/urls.py
@@ -19,13 +19,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from new_booking import views
+from booking import views
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path('accounts/', include("django.contrib.auth.urls")),
     path('newbooking/', views.newbooking ),
     path('existingbooking/', views.existingbooking ),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', views.home),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
